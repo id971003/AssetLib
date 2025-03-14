d17267ce2194b7eb4834a65f8897ca9860a36d5b
id971003 <id971003@gmail.com>
2025-03-14 20:22:15 +0900

    Add CsvReader

---
+++ Lib/CsvReader/CsvLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+
+[Serializable]
+public class GameCsvDataMap : ClassMap<GameCsvData>
+{
+    public GameCsvDataMap()
+    {
+        Map(m => m.type).Name("type");
+        
+        Map(m => m.pName).Name("pName");
+        Map(m => m.pClass).Name("pClass");
+        Map(m => m.text).Name("text");
+        
+        Map(m => m.textColor).Name("textColor");
+        
+        Map(m => m.voiceN).Name("voiceN");
+    }
+}
+
+public class GameCsvData
+{
+    public string type;
+    
+    public string pName;
+    public string pClass;
+    public string text;
+    
+    public string textColor;
+
+    public string voiceN;
+}
---
+++ Lib/CsvReader/CsvManager.cs
@@ -0,0 +1,92 @@
+﻿/*
+[Serializable]
+public class TestData
+{
+    public int Level;
+    public String Name;
+}
+[Serializable]
+public class TestDataMap : ClassMap<GachaDataGameData>
+{
+    public GachaDataGameDataMap()
+    {
+Map(m => m.Level).Name("Level");
+Map(m => m.Name).Name("Name").Default("123");;
+
+    }
+}
+*/
+
+
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+
+
+public class CsvManager : MonoBehaviour
+{
+    public static CsvManager inst;
+
+    private Dictionary<string, TextAsset> _csvFiles = new Dictionary<string, TextAsset>();
+
+    public string _path;
+
+    private void Awake()
+    {
+        if (inst == null)
+        {
+            inst = this;
+            
+            inst.SetCsvData();
+            
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (inst != this) Destroy(gameObject);
+        }
+    }
+    private void SetCsvData()
+    {
+        _csvFiles.Clear();
+        
+        TextAsset[] texts = Resources.LoadAll<TextAsset>(_path); //PATH
+        foreach (var txt in texts)
+        {
+            _csvFiles.Add(txt.name, txt);
+        }
+    }
+    public List<T> LoadCsvData<T, TMap>(string fileName) where TMap : ClassMap<T>
+    {
+        
+        List <T> dataList = new List<T>();
+        TextAsset csvFile = GetTextAsset(fileName);
+        if (csvFile != null)
+        {
+            using (var reader = new StringReader(csvFile.text))
+            using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                
+                csv.Context.RegisterClassMap<TMap>();
+                dataList = csv.GetRecords<T>().ToList();
+                Debug.Log($"Loaded {dataList.Count} records from {fileName} CSV");
+            }
+        }
+        else
+        {
+            Debug.LogError($"CSV file not found with name: {fileName}");
+        }
+
+        return dataList;
+    }
+
+    public TextAsset GetTextAsset(string fileName) => _csvFiles[fileName];
+
+    public string[] GetFileRelatedKeys(string str) => _csvFiles.Keys.Where(key => key.Contains(str)).ToArray();
+}
