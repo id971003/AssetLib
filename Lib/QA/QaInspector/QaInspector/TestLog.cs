5eb8f1f2d480bd84d358d440be58eef53ca69e7c
kim <id971003@gmail.com>
2025-07-07 12:05:11 +0900

    Qa Inspector 추가

---
+++ Lib/QA/QaInspector/QaInspector/TestLog.cs
@@ -0,0 +1,19 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+using UnityEngine;
+
+
+public class TestLog : MonoBehaviour
+{
+    public int GoldValue;
+
+    public int DiaValue;
+
+    public int Trigger_1Value;
+    public int Trigger_2Value;
+
+
+}
